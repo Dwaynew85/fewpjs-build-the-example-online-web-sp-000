cc9d0888bdfd77555a22e3e3d6a5ed86aeb685d0
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-30 04:14:59 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ for(const like of likes) {
         if (error) {
           setError(error);
         } else{
-          console.log(e);
+          console.log(error);
         }
       });
     });
