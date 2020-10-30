2f8ecf07e5edaeab19b56a13b5089e7aa79e4357
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-30 05:15:52 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ function modalShow() {
 
 function setHeart(likeSpan) {
   if ( EMPTY_HEART ) {
-    console.log('empty heart');
+    likeSpan = FULL_HEART;
   }else {
     console.log('full heart');
   }
