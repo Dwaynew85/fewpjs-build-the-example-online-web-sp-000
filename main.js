e21ea046fbb4a086465d10980d2e0ef1b2d193be
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-30 03:24:42 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 let modal = document.getElementById('modal')
-// modal.setAttribute('class', 'hidden')
+modal.hidden = true
 
 let likes = document.querySelectorAll('.like')
 for(const like of likes) {
