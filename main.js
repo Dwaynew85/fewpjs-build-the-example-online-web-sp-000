70108c06d6e2cfc8028bcda7152a8f8cc37f5b4d
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-30 04:49:50 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ for(const like of likes) {
     like.addEventListener('click', function(e) {
       mimicServerCall()
       .then(function(response) {
-        const elem = e
+        console.log(response);
       })
       .catch((error) => {
         setError(error);
